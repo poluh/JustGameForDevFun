cb72f6b537c3f7f923cd8e011721742b32b38d51
Сергей Полухин <sergejpoluhin@MacBook-Air-Sergej.local>
2018-07-08 20:20:10 +0900

    Create classic classes

---
+++ src/main/java/Engine/Geomentry/Pixel.java
@@ -0,0 +1,32 @@
+package Engine.Geomentry;
+
+import javafx.scene.paint.Color;
+
+public class Pixel {
+
+    private int x;
+    private int y;
+    private Color pixelColor;
+
+    public Pixel(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.pixelColor = color;
+    }
+
+    public Pixel(int x, int y) {
+        this(x, y, Color.BLACK);
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Color getPixelColor() {
+        return pixelColor;
+    }
+}
---
+++ src/main/java/Loger/Logger.java
@@ -0,0 +1,52 @@
+package Loger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Time;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Logger {
+
+    private Class classType;
+    private List<String> allLogInfo;
+
+    public Logger(Class classType) {
+        allLogInfo = new ArrayList<>();
+        this.classType = classType;
+    }
+
+    public enum LogType {
+        FINE,
+        NORMAL,
+        MESSAGE,
+        ERROR
+    }
+
+    public void log(String message) {
+        log(message, LogType.NORMAL);
+    }
+
+    public void log(String message, LogType logType) {
+        var normalLog = createLog(message, logType);
+        if (logType == LogType.ERROR) {
+            System.err.println(normalLog);
+        } else {
+            System.out.println(normalLog);
+        }
+        allLogInfo.add(normalLog);
+    }
+
+    public void toFile(String path) throws IOException {
+        Files.write(Paths.get(path), allLogInfo);
+    }
+
+    private String createLog(String message, LogType logType) {
+        var time = LocalTime.now().toString().split("\\.")[0];
+        return classType + " :: " + time + " :: " + logType + " :: " + message;
+    }
+
+}
