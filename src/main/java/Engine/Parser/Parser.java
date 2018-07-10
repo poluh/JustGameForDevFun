8cdaa28b343f0082d312a60fcf9f84d5121ce136
Сергей Полухин <sergejpoluhin@MacBook-Air-Sergej.local>
2018-07-10 21:26:56 +0900

    Create Engine-class

--- src/main/java/Engine/Parser/Parser.java
+++ src/main/java/Engine/Parser/Parser.java
@@ -22,18 +22,16 @@ public class Parser {
     private List<Pixel> parse(List<String> strings) {
         var result = new ArrayList<Pixel>();
         strings.forEach(string -> {
-            for (String stringPixel : string.split("::")) {
-                var positionAndColor = stringPixel.split(";");
-                var x = Integer.parseInt(positionAndColor[0].split(",")[0]);
-                var y = Integer.parseInt(positionAndColor[0].split(",")[1]);
-                var stringColor = positionAndColor[1].split(",");
-                var r = Integer.parseInt(stringColor[0]);
-                var g = Integer.parseInt(stringColor[1]);
-                var b = Integer.parseInt(stringColor[2]);
-                var color = new Color(r, g, b, 1);
-                var pixel = new Pixel(x, y, color);
-                result.add(pixel);
-            }
+            var positionAndColor = string.split(";");
+            var x = Integer.parseInt(positionAndColor[0].split(",")[0]);
+            var y = Integer.parseInt(positionAndColor[0].split(",")[1]);
+            var stringColor = positionAndColor[1].split(",");
+            var r = Integer.parseInt(stringColor[0]);
+            var g = Integer.parseInt(stringColor[1]);
+            var b = Integer.parseInt(stringColor[2]);
+            var color = new Color(r, g, b, 1);
+            var pixel = new Pixel(x, y, color);
+            result.add(pixel);
         });
         return result;
     }
