a17c897521b5786d8bb4919182d3399465c92b9a
Сергей Полухин <sergejpoluhin@MacBook-Air-Sergej.local>
2018-07-11 14:25:21 +0900

    Create class 'BorderRectangle' to check whether the objects

---
+++ src/main/java/engine/physics/BorderRectangle.java
@@ -0,0 +1,53 @@
+package engine.physics;
+
+import java.util.Objects;
+
+class BorderRectangle {
+
+    private class Point {
+
+        private int x;
+        private int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Point)) return false;
+            Point point = (Point) o;
+            return x == point.x &&
+                    y == point.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
+
+    private Point upperRPoint;
+    private Point bottomLPoint;
+    private int height;
+    private int width;
+
+    BorderRectangle(int x, int y, int h, int w) {
+        upperRPoint = new Point(x, y);
+        bottomLPoint = new Point(x + w, y + h);
+        height = h;
+        width = w;
+    }
+
+    public boolean intersect(BorderRectangle other) {
+        return this.pointIsInside(other.bottomLPoint) || this.pointIsInside(other.upperRPoint);
+    }
+
+    private boolean pointIsInside(Point point) {
+        return point.x >= this.upperRPoint.x && point.x <= this.bottomLPoint.x &&
+                point.y <= this.upperRPoint.y && point.y >= this.bottomLPoint.y;
+    }
+
+}
---
+++ src/main/java/engine/physics/Physics.java
@@ -0,0 +1,7 @@
+package engine.physics;
+
+public class Physics  {
+
+
+
+}
