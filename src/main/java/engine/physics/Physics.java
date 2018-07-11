250a37cbe19b7ebd694d2d70ffef4cfab569b4fe
Сергей Полухин <sergejpoluhin@MacBook-Air-Sergej.local>
2018-07-11 22:42:50 +0900

    Create physics class

--- src/main/java/engine/physics/Physics.java
+++ src/main/java/engine/physics/Physics.java
@@ -1,7 +1,19 @@
 package engine.physics;
 
-public class Physics  {
+import engine.gui.Texture;
+import javafx.scene.canvas.Canvas;
 
+public class Physics implements Runnable {
 
+    private Texture texture;
+    private Canvas canvas;
 
+    public Physics(Texture texture) {
+        this.canvas = texture.getCanvas();
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
